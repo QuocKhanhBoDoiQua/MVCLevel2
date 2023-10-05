d5627a3be675b077ca8af54ed58baf40a9210790
QuocKhanh <Qkhanh0290@gmail.com>
2023-10-05 20:28:00 +0700

    first commit

---
+++ Controllers/InformationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAdmin.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Company()
+        {
+            return View();
+        }
+        public IActionResult We()
+        {
+            return View();
+        }
+    }
+}
